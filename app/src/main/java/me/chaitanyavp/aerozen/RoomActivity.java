ef78303eb8266f266e4a5d1df57073e2bca4aa0e
chaitanyavp <chaitanya1998@gmail.com>
2018-07-16 18:34:40 -0400

    Eventlisteners

--- app/src/main/java/me/chaitanyavp/aerozen/RoomActivity.java
+++ app/src/main/java/me/chaitanyavp/aerozen/RoomActivity.java
@@ -1,5 +1,8 @@
 package me.chaitanyavp.aerozen;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -19,6 +22,11 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RoomActivity extends AppCompatActivity {
 
   /**
@@ -35,6 +43,8 @@ public class RoomActivity extends AppCompatActivity {
    * The {@link ViewPager} that will host the section contents.
    */
   private ViewPager mViewPager;
+  private FirebaseDatabase database;
+  private String roomID;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -61,13 +71,55 @@ public class RoomActivity extends AppCompatActivity {
       }
     });
 
+    Intent intent = getIntent();
+    roomID = intent.getStringExtra("room_id");
+    database = FirebaseDatabase.getInstance();
+
+    database.getReference(roomID).addChildEventListener(new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+  }
+
+  private void addBoard(){
+
+  }
+
+  private void removeBoard(){
+
+  }
+
+  private void updateBoard(){
+      
   }
 
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_room, menu);
+//    getMenuInflater().inflate(R.menu.menu_room, menu);
     return true;
   }
 
@@ -149,7 +201,9 @@ public class RoomActivity extends AppCompatActivity {
       return 3;
     }
 
-    public void setCount(){}
+    public void setCount(int pageCount){
+      this.pageCount = pageCount;
+    }
 
   }
 }
